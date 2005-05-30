2b86e987ac89ac15beb53efd09d401820d8530de
shahid.shah <shahid.shah>
2005-05-30 18:00:36 +0000

    Added Resin 2.1 configurations since 3.0 is much slower to work in.

---
+++ pbs/support/bin/resin-2.1-devl-workstation.bat
@@ -0,0 +1,2 @@
+set JAVA_HOME=C:\java\jdk1.5.0_01
+C:\java\app-servers\resin-2.1.14\bin\httpd -Dmedigy.project.home=C:/Projects/Medigy -conf c:\Projects\Medigy\pbs\support\conf\resin-2.1-devl-workstation.conf.xml
