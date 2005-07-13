5b8e9f1f458f011c9be839f25d5c0a6ecd0fd8cf
shahid.shah <shahid.shah>
2005-07-13 20:20:02 +0000

    Added new "AddRecordsPanel" and "QuickSearchPanel" to the home page. These are reusable components that can be placed on any page.

---
+++ pbs/src/java/main/com/medigy/app/pbs/component/AddRecordsPanel.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2005 Your Corporation. All Rights Reserved.
+ */
+package com.medigy.app.pbs.component;
+
+import wicket.markup.html.panel.Panel;
+
+public class AddRecordsPanel extends Panel
+{
+    public AddRecordsPanel(final String id)
+    {
+        super(id);
+    }
+}
---
+++ pbs/src/java/main/com/medigy/app/pbs/component/QuickSearchPanel.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2005 Your Corporation. All Rights Reserved.
+ */
+package com.medigy.app.pbs.component;
+
+import wicket.markup.html.panel.Panel;
+
+public class QuickSearchPanel extends Panel
+{
+    public QuickSearchPanel(final String id)
+    {
+        super(id);
+    }
+}
