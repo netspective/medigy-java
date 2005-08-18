a424e0677fb9f0e8e3aeb5712b470ebca19d35a3
jeremy.hulick <jeremy.hulick>
2005-08-18 15:56:03 +0000

    Schedule service classes.

---
+++ service/src/java/main/com/medigy/service/impl/schedule/ScheduleServiceImpl.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
+package com.medigy.service.impl.schedule;
+
+import com.medigy.service.ServiceVersion;
+import com.medigy.service.schedule.ScheduleService;
+import com.medigy.service.dto.ServiceParameters;
+import com.medigy.service.dto.ServiceReturnValues;
+import com.medigy.service.dto.schedule.ScheduleReturnValues;
+import com.medigy.service.dto.schedule.ScheduleParameters;
+import com.medigy.service.dto.schedule.ScheduleWorksheetItems;
+import com.medigy.scheduler.util.CalendarUtils;
+import com.medigy.scheduler.model.*;
+import com.medigy.scheduler.model.impl.DefaultScheduleManager;
+import com.medigy.scheduler.model.mock.MockScheduleElementProvider;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class ScheduleServiceImpl implements ScheduleService
+{
+    public ScheduleReturnValues getSchedules(final ScheduleParameters parameters)
+    {
+        // TODO: Remove test code, mock objects.
+        Calendar calendar = Calendar.getInstance();
+        CalendarUtils calendarUtils = new CalendarUtils(calendar);
+
+        MockScheduleElementProvider mockScheduleItemProvider = new MockScheduleElementProvider();
+        ScheduleParticipantProvider participantProvider = mockScheduleItemProvider;
+        ScheduleEventProvider eventProvider = mockScheduleItemProvider;
+        ScheduleTemplateProvider templateProvider = mockScheduleItemProvider;
+
+        ScheduleManager scheduleManager = new DefaultScheduleManager(calendarUtils, participantProvider, eventProvider, templateProvider);
+
+        Date beginDate = calendarUtils.createDate(Calendar.JANUARY, 1, 2004);
+        Date endDate = calendarUtils.createDate(Calendar.JANUARY, 1, 2004);
+
+        int beginDay = calendarUtils.getJulianDay(beginDate);
+        int endDay = calendarUtils.getJulianDay(endDate);
+
+        ScheduleEvents scheduleEvents = eventProvider.getScheduledEvents(scheduleManager, beginDate, endDate);
+        final ScheduleEvent[] scheduleEventsList = scheduleEvents.getScheduleEvents();
+
+        return new ScheduleReturnValues()
+        {
+            public List<ScheduleWorksheetItems> getItems()
+            {
+                return null;
+            }
+
+            public ScheduleParameters getParameters()
+            {
+                return parameters;
+            }
+
+            public String getErrorMessage()
+            {
+                return null;
+            }
+        };
+    }
+
+    public ServiceReturnValues createErrorResponse(final ServiceParameters params, final String errorMessage)
+    {
+        return new ScheduleReturnValues()
+        {
+            public List<ScheduleWorksheetItems> getItems()
+            {
+                return null;
+            }
+
+            public ScheduleParameters getParameters()
+            {
+                return (ScheduleParameters) params;
+            }
+
+            public String getErrorMessage()
+            {
+                return errorMessage;
+            }
+        };
+    }
+
+    public ServiceVersion[] getSupportedServiceVersions()
+    {
+        return new ServiceVersion[0];
+    }
+
+    public String[] isValid(ServiceParameters parameters)
+    {
+        return null;
+    }
+}
