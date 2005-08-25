039e87eb73b563b6bf97101188b367a0a099eb81
aye.thu <aye.thu>
2005-08-25 13:57:12 +0000

    Added simple page to display Patient full name for now.

---
+++ service/src/java/main/com/medigy/service/impl/person/PatientProfileServiceImpl.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
+package com.medigy.service.impl.person;
+
+import com.medigy.persist.model.person.Person;
+import com.medigy.service.AbstractService;
+import com.medigy.service.ServiceVersion;
+import com.medigy.service.dto.ServiceParameters;
+import com.medigy.service.dto.ServiceReturnValues;
+import com.medigy.service.dto.person.PatientProfileParameters;
+import com.medigy.service.dto.person.PatientProfileReturnValues;
+import com.medigy.service.person.PatientProfileService;
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+public class PatientProfileServiceImpl extends AbstractService   implements PatientProfileService
+{
+    public PatientProfileServiceImpl(final SessionFactory sessionFactory)
+    {
+        super(sessionFactory);
+    }
+
+    public PatientProfileReturnValues getProfile(final PatientProfileParameters params)
+    {
+        final Session session = getSession();
+        final Criteria criteria = session.createCriteria(Person.class);
+        criteria.setFetchMode("identifiers", FetchMode.JOIN);
+        criteria.setFetchMode("ethnicities", FetchMode.JOIN);
+        criteria.setFetchMode("insurancePolicies", FetchMode.JOIN);
+
+
+        criteria.add(Restrictions.eq("partyId", params.getPatientId()));
+        final Person patient = (Person) criteria.uniqueResult();
+
+        return new PatientProfileReturnValues() {
+            public Person getPatient()
+            {
+                return patient;
+            }
+
+            public String getErrorMessage()
+            {
+                return null;
+            }
+
+            public ServiceParameters getParameters()
+            {
+                return params;
+            }
+        };
+    }
+
+    public ServiceVersion[] getSupportedServiceVersions()
+    {
+        return new ServiceVersion[0];
+    }
+
+    public String[] isValid(ServiceParameters parameters)
+    {
+        return new String[0];
+    }
+
+    public ServiceReturnValues createErrorResponse(final ServiceParameters params, final String errorMessage)
+    {
+        return null;
+    }
+}
